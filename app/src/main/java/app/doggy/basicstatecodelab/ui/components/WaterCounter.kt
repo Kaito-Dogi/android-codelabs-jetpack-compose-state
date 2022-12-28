c51046cd19a60d7aa19b4000d99a3ea8679d9ec1
Kaito-Dogi <kendobu0405@gmail.com>
2022-12-29 05:39:59 +0900

    :sparkles: count を State にし、 Layout Inspector で UI の状態を確認

--- app/src/main/java/app/doggy/basicstatecodelab/ui/components/WaterCounter.kt
+++ app/src/main/java/app/doggy/basicstatecodelab/ui/components/WaterCounter.kt
@@ -1,9 +1,15 @@
 package app.doggy.basicstatecodelab.ui.components
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -15,14 +21,26 @@ import app.doggy.basicstatecodelab.ui.theme.BasicStateCodelabTheme
 internal fun WaterCounter(
   modifier: Modifier = Modifier,
 ) {
-  val count = 0
-  Text(
-    text = stringResource(
-      id = R.string.water_counter_text,
-      count,
-    ),
-    modifier = modifier.padding(16.dp),
-  )
+  var count by remember {
+    mutableStateOf(0)
+  }
+  Column(modifier = modifier.padding(16.dp)) {
+    if (count > 0) {
+      Text(
+        text = stringResource(
+          id = R.string.water_counter_text,
+          count,
+        ),
+      )
+    }
+    Button(
+      onClick = { count++ },
+      modifier = Modifier.padding(top = 8.dp),
+      enabled = count < 10,
+    ) {
+      Text(text = stringResource(id = R.string.add_button_text))
+    }
+  }
 }
 
 @Preview
