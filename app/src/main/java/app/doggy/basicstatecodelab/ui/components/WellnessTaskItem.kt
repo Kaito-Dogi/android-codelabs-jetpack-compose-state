07cc1eded3c899afe9d7fda70829d89e0043c2dc
Kaito-Dogi <kendobu0405@gmail.com>
2023-01-01 05:55:40 +0900

    :recycle: WellnessTaskItem で状態ホイスティング

--- app/src/main/java/app/doggy/basicstatecodelab/ui/components/WellnessTaskItem.kt
+++ app/src/main/java/app/doggy/basicstatecodelab/ui/components/WellnessTaskItem.kt
@@ -3,6 +3,7 @@ package app.doggy.basicstatecodelab.ui.components
 import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Checkbox
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.Surface
@@ -10,6 +11,10 @@ import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -21,6 +26,23 @@ import app.doggy.basicstatecodelab.ui.theme.BasicStateCodelabTheme
 @Composable
 internal fun WellnessTaskItem(
   @StringRes taskResId: Int,
+  modifier: Modifier = Modifier,
+) {
+  var checked by remember { mutableStateOf(false) }
+
+  WellnessTaskItem(
+    taskResId = taskResId,
+    checked = checked,
+    onCheckedChange = { newValue -> checked = newValue },
+    onCloseButtonClick = {},
+  )
+}
+
+@Composable
+private fun WellnessTaskItem(
+  @StringRes taskResId: Int,
+  checked: Boolean,
+  onCheckedChange: (Boolean) -> Unit,
   onCloseButtonClick: () -> Unit,
   modifier: Modifier = Modifier,
 ) {
@@ -33,6 +55,10 @@ internal fun WellnessTaskItem(
       modifier = Modifier
         .weight(1f)
         .padding(start = 16.dp),
+    )
+    Checkbox(
+      checked = checked,
+      onCheckedChange = onCheckedChange,
     )
     IconButton(onClick = onCloseButtonClick) {
       Icon(
@@ -50,6 +76,8 @@ private fun WellnessTaskItemPreview() {
     Surface {
       WellnessTaskItem(
         taskResId = R.string.wellness_task_item_text,
+        checked = false,
+        onCheckedChange = {},
         onCloseButtonClick = {},
       )
     }
